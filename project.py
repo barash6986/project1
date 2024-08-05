615f537ca225410ef2de801f2a2391ca991f1f80
barash6986 <timselyutin1@gmail.com>
2024-08-05 18:12:54 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -8,10 +8,6 @@ import threading
 bot = telebot.TeleBot('7429781312:AAFzIFy7dvJLQtGbXaAfXZwms9TFjXNmpbs')
 
 
-# thr = threading.Thread(target=chrome)
-# thr.start()
-
-
 @bot.message_handler(commands=['start'])
 def start(message):
     kb = types.InlineKeyboardMarkup()
@@ -28,30 +24,32 @@ def start(message):
 def callback(query):
     print(query.data)
     if query.data == 'Chrome':
-        # chrome(query)
         threading.Thread(target=chrome, args=(query,)).start()
     elif query.data == 'Screenshot':
         threading.Thread(target=screenshot, args=(query,)).start()
-
     elif query.data == 'Python':
-        bot.edit_message_text(message_id=query.message.id, chat_id=query.from_user.id, text="Отправьте текст для запуска",
-                              reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='Menu')))
+        bot.edit_message_text(message_id=query.message.id, chat_id=query.from_user.id,
+                              text="Отправьте текст для запуска",
+                              reply_markup=types.InlineKeyboardMarkup().add(
+                                  types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='Menu')))
         bot.register_next_step_handler(query.message, code)
     elif query.data == 'Menu':
-        bot.delete_message(query.message.chat.id, query.message.message_id)
         menu(query.message)
 
 
 def screenshot(query):
     bot.delete_message(query.message.chat.id, query.message.message_id)
-    bot.send_photo(chat_id=query.message.chat.id, photo=pyautogui.screenshot())
-    menu(query.message)
+    bot.send_photo(chat_id=query.message.chat.id, photo=pyautogui.screenshot(),
+                   reply_markup=types.InlineKeyboardMarkup().add(
+                       types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='Menu')))
+
 
 def chrome(query):
     os.startfile('C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe')
     bot.edit_message_text(message_id=query.message.id, chat_id=query.message.chat.id,
                           text="Запустил приложение " + query.data,
-                          reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='Menu')))
+                          reply_markup=types.InlineKeyboardMarkup().add(
+                              types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='Menu')))
 
 
 def code(message):
@@ -60,10 +58,13 @@ def code(message):
     try:
         result = subprocess.run(['python', 'tets.py'], capture_output=True, text=True, check=True, encoding='utf-8')
         if result.stdout != "":
-            bot.send_message(message.chat.id, result.stdout)
+            bot.send_message(message.chat.id, result.stdout,
+                             reply_markup=types.InlineKeyboardMarkup().add(
+                                 types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='Menu')))
     except subprocess.CalledProcessError as e:
-        bot.send_message(message.chat.id, e.stderr)
-    menu(message)
+        bot.send_message(message.chat.id, e.stderr,
+                         reply_markup=types.InlineKeyboardMarkup().add(
+                             types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='Menu')))
 
 
 def menu(message):
@@ -72,9 +73,13 @@ def menu(message):
     btn2 = types.InlineKeyboardButton(text='Сделать скриншот', callback_data='Screenshot')
     btn3 = types.InlineKeyboardButton(text='Запустить код', callback_data='Python')
     kb.add(btn1, btn2, btn3)
-    bot.send_message(chat_id=message.chat.id,
-                     text='Главное меню',
-                     reply_markup=kb)
+    if message.content_type!='text':
+        bot.delete_message(message.chat.id, message.message_id)
+        bot.send_message(chat_id=message.chat.id,
+                         text='Главное меню',
+                         reply_markup=kb)
+    else:
+        bot.edit_message_text(message_id=message.id, chat_id=message.chat.id, text='Главное меню', reply_markup=kb)
 
 
 if __name__ == '__main__':
